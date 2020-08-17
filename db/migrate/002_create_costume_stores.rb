bd9642243a7dda7a853ebe2d8f7392d88cc60edf
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-08-17 02:48:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,17 +2,15 @@
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
-  def change
-      create_table :costume_stores do |t|
-        t.string :name
-        t.string :location
-        t.integer :costume_inventory
-        t.integer :num_of_employees
-        t.boolean :still_in_business
-        t.datetime :opening_time
-        t.datetime :closing_time
-
-        t.timestamps
+    def change
+        create_table :costume_stores do |t|
+            t.string :name
+            t.string :location
+            t.integer :costume_inventory
+            t.integer :num_of_employees
+            t.boolean :still_in_business
+            t.datetime :opening_time
+            t.datetime :closing_time
+        end
     end
-  end
 end
